54245fbd0c0eee19aa1ffcb79a3d574ecd62acdb
jansromi <rrobertjansson@gmail.com>
2023-12-27 11:57:10 +0200

    Start to work on User-class

---
+++ src/core/user.py
@@ -0,0 +1,6 @@
+from src.core.entries import Entries
+from src.core.password import Password
+
+class User:
+    def __init__(self, id: int, entries: Entries, password: Password) -> None:
+        pass
